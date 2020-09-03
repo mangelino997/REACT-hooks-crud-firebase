07cd279d446d26033adc52ebc99c6f7830be31c3
Marcio <marcio@draimo.com>
2020-09-02 22:04:43 -0300

    Updates

---
+++ precache-manifest.698ebd309705b6f6a5c000112bc57e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580482c5a5eda67c5d779a1b8aa3c31f",
+    "url": "/REACT-hooks-crud-firebase/index.html"
+  },
+  {
+    "revision": "c8727842ff8c7aa81e4c",
+    "url": "/REACT-hooks-crud-firebase/static/css/main.36737555.chunk.css"
+  },
+  {
+    "revision": "c0bb43d0a3c3c1aca6ef",
+    "url": "/REACT-hooks-crud-firebase/static/js/2.21fee720.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/REACT-hooks-crud-firebase/static/js/2.21fee720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8727842ff8c7aa81e4c",
+    "url": "/REACT-hooks-crud-firebase/static/js/main.1fa1feb0.chunk.js"
+  },
+  {
+    "revision": "63f97aeba36cb279b7af",
+    "url": "/REACT-hooks-crud-firebase/static/js/runtime-main.677defe7.js"
+  }
+]);
